cddb51c5a5a7c12a9e38453fc5ac082e79096e71
YagoRGM <yago.roberto2008@gmail.com>
2024-09-19 16:08:58 -0300

    Exercicio 1 pronto

--- exercicio1/cadastrar.php
+++ exercicio1/cadastrar.php
@@ -3,10 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro</title>
     <link rel="stylesheet" href="cadastrar.css">
+    <script src="script.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <div id="container">
+        <?php
+        
+        echo "<h2>Cliente cadastrado com sucesso!</h2><br>";
+        echo "<ul><li><strong>Bem vindo!,</strong> " . $_POST["nome"] . "</li><br>";
+        echo "<li><strong>Email:</strong> " . $_POST["email"] . "</li><br>";
+        echo "<li><strong>Telefone: </strong>" . $_POST["tel"] . "</li></ul>";
+        ?>   
+    </div>
+
 </body>
 </html>
---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+Swal.fire({
+    title: "Cliente cadastrado!",
+    text: "Seu cadastro foi concluído com sucesso!",
+    icon: "success"
+  });
